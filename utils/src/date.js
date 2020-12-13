2eb3efc9b8e619f6619a3c2312134d1e4f5adb35
huzc02 <huzc02@mingyuanyun.com>
2020-12-13 12:56:17 +0800

    添加utils

--- utils/src/date.js
+++ utils/src/date.js
@@ -1,7 +1,7 @@
-export const getTimeStamp = (by) => {
+export const getTimeStamp = (type) => {
     let time = new Date().getTime();
     let res = 0;
-    switch(by) {
+    switch(type) {
         case 'seconds': res = parseInt(time / 1e3); break;
         case 'minutes': res = parseInt(time / 6e5); break;
         case 'hours': res = parseInt(time / 3.6e6); break;
